ffa826423843ef1505812bba1fac3b276073f722
bilal-bin-yar <bilalbinyar@neurog.ai>
2023-04-04 11:09:03 +0500

    Canonical Tag Added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -28,6 +28,8 @@ const Home = () => {
           name="description"
           content="An economic framework for the prediction/forecast of Bitcoin and other cryptocurrencies using AI and ML models with a comprehensive evaluation of back and forward tests."
         />
+        {/* CANONICAL TAG */}
+        <link rel="canonical" href="https://zerotheorem.com/"/>
       </Helmet>
       {/* <BtcData /> */}
       {/* <Documentation /> */}
