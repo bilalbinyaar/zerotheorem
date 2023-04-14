28e0cd2843d7a6d494566a0b869ebbcd5de67350
bilal-bin-yar <bilalbinyar@neurog.ai>
2023-04-14 16:19:46 +0500

    Performance Page Grid Layout

--- src/graphs/PerformanceBarChart.js
+++ src/graphs/PerformanceBarChart.js
@@ -1,39 +1,11 @@
-import React, { useState, useEffect, useRef } from "react";
-import CanvasJSReact from "../canvasjs.stock.react";
-import { useStateContext } from "../ContextProvider";
+import React from "react";
 
-const CanvasJS = CanvasJSReact.CanvasJS;
-const CanvasJSStockChart = CanvasJSReact.CanvasJSStockChart;
 
-const PerformanceBarChart = () => {
-    const data = [
-        { label: "A", y: 10 },
-        { label: "B", y: -5 },
-        { label: "C", y: 15 },
-        { label: "D", y: -20 },
-        { label: "E", y: 5 },
-    ];
-  const options = {
-  axisY: {
-    includeZero: false,
-    minimum: -30,
-    maximum: 30,
-    interval: 10,
-    labelFormatter: function (e) {
-      return Math.abs(e.value);
-    },
-  },
-  data: [
-    {
-      type: "bar",
-      dataPoints: data,
-    },
-  ],
-};
-
+const PerformanceBarChart = (props) => {
+   
   return (
     <div>
-        <CanvasJSStockChart options={options} />
+       <h1>Negative Bar Chart</h1>
     </div>
   );
 };
--- src/pages/Performance.js
+++ src/pages/Performance.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Portfolio from '../components/portfolio/Portfolio'
 import PerformanceGraphs from '../components/performanceGraph/PerformanceGraphs'
+import PerformanceDataGrid from '../components/performanceGrid/PerformanceDataGrid'
 
 const Performance = () => {
   return (
     <React.Fragment>
         <Portfolio />
         <PerformanceGraphs />
+        <PerformanceDataGrid />
     </React.Fragment>
   )
 }
