51afa6c5bda1ecca87fa85ace929c7c50557cc68
bilal-bin-yar <bilalbinyar@neurog.ai>
2023-05-18 13:39:50 +0500

    Live Pnl Section for web has been developed

--- src/components/livepnl/LivePNL.js
+++ src/components/livepnl/LivePNL.js
@@ -6,9 +6,45 @@ const LivePNL = () => {
   return (
     <div className='live-pnl'>
         <div className='container'>
-            <h3>
+            <h2>
             Live PNL Bar
-            </h3>
+            </h2>
+
+            <div className='live-pnl-stats-div'>
+                <div className='overall-live overall-live-stats'>
+                    <h3>Overall</h3>
+                    <h3 className='live-stats'>2.5%</h3>
+                </div>
+
+                <div className='divider-div-pnl-live'></div>
+
+                <div className='strategies-live-stats'>
+                    <div className='overall-live strategy-live-stats'>
+                        <h3>Strategy_1</h3>
+                        <h3 className='live-stats'>2.5%</h3>
+                    </div>
+
+                    <div className='overall-live strategy-live-stats'>
+                        <h3>Strategy_2</h3>
+                        <h3 className='live-stats'>2.5%</h3>
+                    </div>
+
+                    <div className='overall-live strategy-live-stats'>
+                        <h3>Strategy_3</h3>
+                        <h3 className='live-stats'>2.5%</h3>
+                    </div>
+
+                    <div className='overall-live strategy-live-stats'>
+                        <h3>Strategy_4</h3>
+                        <h3 className='live-stats'>2.5%</h3>
+                    </div>
+
+                    <div className='overall-live strategy-live-stats'>
+                        <h3>Strategy_5</h3>
+                        <h3 className='live-stats'>2.5%</h3>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
   )
